c83690f5f49878d1fec79dfe870983c0e1259a25
Manoel Junior <manoeljunior06avelino@gmail.com>
2022-08-10 15:53:22 -0300

    Usando banco ATLAS

--- APP/data/bd.js
+++ APP/data/bd.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
- mongoose.connect(
+/*  mongoose.connect(
     `mongodb://${process.env.MONGO_HOST}/`+
     `${process.env.MONGO_DATABASE}`
   );
+ */
 
-
-/* main().catch(err => console.log(err));
+main().catch(err => console.log(err));
 
 async function main() {
     await mongoose.connect(
     `mongodb+srv://${process.env.ATLAS_USER}:`+
     `${process.env.ATLAS_PASSWORD}`+
     `@${process.env.ATLAS_HOST}/?retryWrites=true&w=majority`);
-  } */
+  }
 
 console.log('Sucess conect mongoDB');
 
